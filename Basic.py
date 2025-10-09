da4732510df083cf0d3d3dd61e6c00a5b155af22
kapil212a <amangupta74086@gmail.com>
2025-10-09 13:50:41 +0530

    change

--- Basic.py
+++ Basic.py
@@ -1,4 +1,25 @@
 m = 40
 n = 50
 x = m/n
-print("fraction  form of " , m ,"/" , n)
+print("fraction form of "  , m ,"/" , n)
+
+
+list1 = [1,2,3,4,5]
+list2 = []
+
+for i in list1:
+    list2.insert(0,i)
+print(list2)
+
+class Fraction:
+    def __init__(self , m ,n):
+        self.numer = m
+        self.deno = n
+
+    def result(self):
+        print("fraction:", self.numer, "/", self.deno)
+        print("Numerator:" ,self.numer)
+        print("Denominator:", self.deno)
+
+obj1 = Fraction(40,50)
+obj1.result()
